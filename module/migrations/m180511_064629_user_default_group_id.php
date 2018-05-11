175818efcf292f7e7e639ce33bf5894e6c5ec878
Denis Po <denis.pogrebnoy@gmail.com>
2018-05-11 10:03:32 +0300

    n/

--- module/migrations/m180511_064629_user_default_group_id.php
+++ module/migrations/m180511_064629_user_default_group_id.php
@@ -9,14 +9,13 @@ use yii\db\Migration;
  */
 class m180511_064629_user_default_group_id extends Migration
 {
-	
+
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-    	$this->alterColumn('{{%user}}', 'group_id', $this->integer(10)->unsigned()->notNull()->defaultValue(2));
-
+    	$this->alterColumn('{{%user}}', 'group_id', $this->integer(10)->unsigned()->notNull()->defaultValue(4));
     }
 
     /**
